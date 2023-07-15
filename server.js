db27ce7ba504a807c47460c7a1344c8bd2670fc2
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-07-15 13:29:10 +0600

    complete crud with database

--- server.js
+++ server.js
@@ -1,82 +1,106 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri , {useNewUrlParser: true})
-  .then(() => console.log('Connected!'))
-  .catch(() => console.log('Not connected'))
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("Connected!"))
+  .catch(() => console.log("Not connected"));
 
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
-const userSchema = new mongoose.Schema({
-  fname: String,
-  lname: String,
-  email: String,
-  age: Number
-});
+const userSchema = new mongoose.Schema(
+  {
+    fname: String,
+    lname: String,
+    email: String,
+    age: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
-  res.json({ msg: "I am connected" })
+  res.json({ msg: "I am connected" });
 });
 
-const users = [];
-let id = 0;
-app.post("/users", (req, res) => {
-  const user = req.body;
-  user.id = ++id;
-  users.push(user);
-  res.json(users);
+app.post("/users", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: "Something is wrong with the server" });
+  }
 });
 
-app.get("/users", (req, res) => {
-  res.json(users);
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: "Something is wrong with the server" });
+  }
 });
-app.get("/users/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const user = users.find((user) => user.id === id);
+
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: "user not found"});
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: "Something is wrong with the server" });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  
+  try {
+    const id = req.params.id;
+  const body = req.body;
+  const user = await User.findByIdAndUpdate(id,body,{new:true});
   if (user) {
     res.json(user);
   } else {
-    res.status(404).json({ message: "user missing" })
-  } 
+    res.status(404).json({ message: "user not found" });
+  }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: "Something is wrong with the server" });
+  }
 });
 
-app.put('/users/:id', (req,res) => {
-  const id = req.params.id
-  const body = req.body
-
-  const user = users.find((u) => u.id == id)
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+  const user = await User.findByIdAndDelete(id);
   if (user) {
-    user.fname = body.fname
-    user.lname = body.lname
-    res.json(user);
+    res.json([user,{massage: 'user deleted'}]);
   } else {
-    res.status(404).json({ message: "user missing" });
-  } 
-
-})
-
-app.delete('/users/:id', (req,res) => {
-  const id = req.params.id
-  const userIndex = users.findIndex((u) => u.id == id)
-  if (userIndex) {
-    users.splice(userIndex,1)
-    res.json({massage: "user deleted"});
-  } else {
-    res.status(404).json({ message: "user missing" });
-  } 
-
-})
+    res.status(404).json({ message: "user not found" });
+  }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: "Something is wrong with the server" });
+  }
+});
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`I am running on port ${port}`);
 });
- 
