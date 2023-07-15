9e8efc08f070bd4555d044c872c17cb00adc5d76
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-07-16 00:15:59 +0600

    complete bcrypt for hash my password

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const uri = process.env.MONGODB_URI;
 
@@ -19,6 +21,7 @@ const userSchema = new mongoose.Schema(
     fname: String,
     lname: String,
     email: String,
+    password: String,
     age: Number,
   },
   {
@@ -29,14 +32,46 @@ const userSchema = new mongoose.Schema(
 const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
-  res.json({ msg: "I am connected" });
+  res.status(200).json({ msg: "I am connected" });
 });
 
 app.post("/users", async (req, res) => {
   try {
-    const user = new User(req.body);
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const password = hash;
+    const userObj = {
+      fname: req.body.fname,
+      lname: req.body.lname,
+      email: req.body.email,
+      age: req.body.age,
+      password: password,
+    };
+    const user = new User(userObj);
     await user.save();
     res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ massage: "Something is wrong with the server" });
+  }
+});
+app.post("/users/login", async (req, res) => {
+  try {
+    const {email,password} = req.body;
+    const user = await User.findOne({email: email});
+    if(!user){
+      res.status(404).json({ message: "user not found" });
+    } else {
+      const isValidPassword = bcrypt.compareSync(password, user.password); 
+      if(!isValidPassword){
+        res.status(401).json({ message: "wrong password" });
+      } else {
+        const token = jwt.sign({email: user.email, id: user._id },'secret');
+        const userObj = user.toJSON();
+        userObj['accessToken'] = token;
+        res.status(200).json(userObj);
+      }
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ massage: "Something is wrong with the server" });
@@ -46,7 +81,7 @@ app.post("/users", async (req, res) => {
 app.get("/users", async (req, res) => {
   try {
     const users = await User.find({});
-    res.json(users);
+    res.status(200).json(users);
   } catch (error) {
     console.error(error);
     res.status(500).json({ massage: "Something is wrong with the server" });
@@ -58,9 +93,9 @@ app.get("/users/:id", async (req, res) => {
     const id = req.params.id;
     const user = await User.findById(id);
     if (user) {
-      res.json(user);
+      res.status(200).json(user);
     } else {
-      res.status(404).json({ message: "user not found"});
+      res.status(404).json({ message: "user not found" });
     }
   } catch (error) {
     console.error(error);
@@ -69,16 +104,15 @@ app.get("/users/:id", async (req, res) => {
 });
 
 app.put("/users/:id", async (req, res) => {
-  
   try {
     const id = req.params.id;
-  const body = req.body;
-  const user = await User.findByIdAndUpdate(id,body,{new:true});
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ message: "user not found" });
-  }
+    const body = req.body;
+    const user = await User.findByIdAndUpdate(id, body, { new: true });
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "user not found" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ massage: "Something is wrong with the server" });
@@ -88,12 +122,12 @@ app.put("/users/:id", async (req, res) => {
 app.delete("/users/:id", async (req, res) => {
   try {
     const id = req.params.id;
-  const user = await User.findByIdAndDelete(id);
-  if (user) {
-    res.json([user,{massage: 'user deleted'}]);
-  } else {
-    res.status(404).json({ message: "user not found" });
-  }
+    const user = await User.findByIdAndDelete(id);
+    if (user) {
+      res.status(200).json([user, { massage: "user deleted" }]);
+    } else {
+      res.status(404).json({ message: "user not found" });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ massage: "Something is wrong with the server" });
