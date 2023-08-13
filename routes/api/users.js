9ab889e808db8678db0419d57d63cde8edf806f0
Minhaj28 <mdminhajuddin.official@gmail.com>
2023-08-13 20:19:49 +0600

    add validator and comment

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcrypt");
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
@@ -6,44 +7,77 @@ const authenticateToken = require("../../middleware/auth");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
-  try {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    const password = hash;
-    const userObj = {
-      fname: req.body.fname,
-      lname: req.body.lname,
-      email: req.body.email,
-      age: req.body.age,
-      password: password,
-    };
-    const user = new User(userObj);
-    await user.save();
-    res.status(201).json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ massage: "Something is wrong with the server" });
+router.post(
+  "/",
+  [
+    authenticateToken,
+    body("fname", "fname is required").notEmpty(),
+    body("lname", "lname is required").notEmpty(),
+    body("email", "please enter a valid email").notEmpty().isEmail(),
+    body("age", "age is required").optional().isNumeric(),
+    body(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+      const password = hash;
+      const userObj = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        age: req.body.age,
+        password: password,
+      };
+      const user = new User(userObj);
+      await user.save();
+      res.status(201).json(user);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ massage: "Something is wrong with the server" });
+    }
   }
-});
+);
 
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password, type, refreshToken } = req.body;
-    if (!type) {
-      res.status(404).json({ message: "Type is not defined" });
-    } else {
+/**
+  * @author Md Minhaj Uddin
+  * @desc Login a user
+  * @param {email, password, type, refreshToken}
+  * @return {accessToken , refreshToken , user}
+*/
+
+router.post(
+  "/login",
+  [
+    body("type", "type must be email or refresh").isIn(["email", "refresh"]),
+    body("email", "please enter a valid email").notEmpty().isEmail(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { email, password, type, refreshToken } = req.body;
+
       if (type == "email") {
         await handleEmailLogin(email, res, password);
       } else {
         handleRefreshLogin(refreshToken, res);
       }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ massage: "Something is wrong with the server" });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ massage: "Something is wrong with the server" });
   }
-});
+);
 
 router.get("/profile", authenticateToken, async (req, res) => {
   try {
@@ -60,7 +94,7 @@ router.get("/profile", authenticateToken, async (req, res) => {
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", authenticateToken, async (req, res) => {
   try {
     const users = await User.find({});
     res.status(200).json(users);
@@ -85,23 +119,37 @@ router.get("/:id", authenticateToken, async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const body = req.body;
-    const user = await User.findByIdAndUpdate(id, body, { new: true });
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ message: "user not found" });
+router.put(
+  "/:id",
+  [
+    authenticateToken,
+    body("fname", "fname is required").notEmpty(),
+    body("lname", "lname is required").notEmpty(),
+    body("email", "please enter a valid email").notEmpty().isEmail(),
+    body("age", "age is required").optional().isNumeric(),
+  ],
+  async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const id = req.params.id;
+      const body = req.body;
+      const user = await User.findByIdAndUpdate(id, body, { new: true });
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "user not found" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ massage: "Something is wrong with the server" });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ massage: "Something is wrong with the server" });
   }
-});
+);
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", authenticateToken, async (req, res) => {
   try {
     const id = req.params.id;
     const user = await User.findByIdAndDelete(id);
@@ -137,6 +185,13 @@ function handleRefreshLogin(refreshToken, res) {
     });
   }
 }
+
+/**
+  * @author Md Minhaj Uddin
+  * @desc Handle email login
+  * @param email, password
+  * @return accessToken , refreshToken , user
+*/
 
 async function handleEmailLogin(email, res, password) {
   const user = await User.findOne({ email: email });
